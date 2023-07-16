d347847b30a745853995fb72d8288c1c1ad09aa3
Prerit Mishra <preritmishra2062670@gmail.com>
2023-07-16 23:36:34 +0530

    Last commit

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -14,29 +14,29 @@ const HeaderCartButton = (props) => {
     return curNumber + item.amount;
   }, 0);
 
-  const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
-
+  
   useEffect(() => {
     if (items.length === 0) {
       return;
     }
     setBtnIsHighlighted(true);
-
+    
     const timer = setTimeout(() => {
       setBtnIsHighlighted(false);
     }, 300);
-
+    
     return () => {
       clearTimeout(timer);
     };
   }, [items]);
-
+  
+  const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
   return (
     <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
-      <span>Your Cart</span>
+      <span className={classes['cart-text']}>Your Cart</span>
       <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
